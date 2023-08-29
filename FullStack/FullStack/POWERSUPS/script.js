fa8f2f131dd4b57130ac72f18f215b5c816c43a9
MarcelloLM <106484381+MarcelloLM@users.noreply.github.com>
2023-08-29 08:17:14 -0300

    Update script.js

--- FullStack/FullStack/POWERSUPS/script.js
+++ FullStack/FullStack/POWERSUPS/script.js
@@ -1,63 +1,65 @@
+// vamos criar um objeto para conseguir manipular as variaveis com mais facilidade
 const catalogo = [
     {
         id: 1, 
         nome: "AMINO POWDER",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 67.50,
         Imagem: "amino.png", 
     },
     {
         id: 2,
         nome: "ARGININA",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 30,
         Imagem: "arginina.png", 
     },
     {
         id: 3, 
         nome: "CREATINA",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 100,
         Imagem: "creatina.png", 
     },
     {
         id: 4,
         nome: "CREME DE AVELA",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 45,
         Imagem: "cremedeavela.png",
     },
     {
         id: 5,
         nome: "HAZE",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 105, 
         Imagem: "haze.png",
     },
     {
         id: 6,
         nome: "INSANITY",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 100,
         Imagem: "insanity.png"
     },
     {
         id: 7,
         nome: "MULTIVITAMINICO",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 45,
         Imagem: "multi.png"
     },
     {
         id: 8, 
         nome: "WHEY PROTEIN 80%",
-        marca: "POWER SUPS",
+        marca: "POWERSUPS",
         preco: 99,
         Imagem: "whey.png"
     }
 
 ]
-
+// Aqui nos podemos deixar em um laço para conseguir acessar todos os produtos ao mesmo momento
+// Nao tendo que criar um produto por um 
 for (const produtoCatalogo of catalogo){
     const cartaoProduto = `<div id="card-produto1">
 <img src="img/${produtoCatalogo.Imagem}" alt="Produto 1 do powersups.">
@@ -67,6 +69,7 @@ for (const produtoCatalogo of catalogo){
 <button>Adicionar ao carrinho</button>
 </div>`; 
 
+// Aqui estamos acessando o HTML atraves do javascript
 document.getElementById("containter-produto").innerHTML += cartaoProduto; 
 };
 
